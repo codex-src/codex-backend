430238df37000b21edf2badfd0b7fd710c66d97d
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-14 05:18:22 +0900

    Added basic GraphQL integration

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	cloud.google.com/go/firestore v1.1.1 // indirect
 	firebase.google.com/go v3.12.0+incompatible
 	github.com/GoogleCloudPlatform/cloudsql-proxy v0.0.0-20200226203936-887a30e53f59 // indirect
+	github.com/graph-gophers/graphql-go v0.0.0-20200309224638-dae41bde9ef9 // indirect
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a
 	google.golang.org/api v0.20.0
 )
--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
+	graphql "github.com/graph-gophers/graphql-go"
 	"google.golang.org/api/option"
 
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
@@ -18,13 +20,26 @@ import (
 )
 
 var (
+	// Postgres
 	db *sql.DB
 
+	// Firebase
 	app    *firebase.App
 	client *auth.Client
+
+	// GraphQL
+	schema *graphql.Schema
 )
 
 const idToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjhjZjBjNjQyZDQwOWRlODJlY2M5MjI4ZTRiZDc5OTkzOTZiNTY3NDAiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiWmF5ZGVrIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BT2gxNEdpRENxc1Y5bmU1MjdZNnpwejUxOE40VHNNY3J0VU5TNWtubXdSNXV3IiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2NvZGV4LWVmMzIyIiwiYXVkIjoiY29kZXgtZWYzMjIiLCJhdXRoX3RpbWUiOjE1ODQxMjQyMTEsInVzZXJfaWQiOiJ0WG14SGJpUElDYkRCdDFPckFFbHJUSERTQTQyIiwic3ViIjoidFhteEhiaVBJQ2JEQnQxT3JBRWxyVEhEU0E0MiIsImlhdCI6MTU4NDEyNDIxMywiZXhwIjoxNTg0MTI3ODEzLCJlbWFpbCI6InpheWRla2RvdGNvbUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjEwNzI1Mjk3Mzk2NDE0NDA1NzU3MyJdLCJlbWFpbCI6WyJ6YXlkZWtkb3Rjb21AZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZ29vZ2xlLmNvbSJ9fQ.pOsCxDnGDNedKB0Xs07-MWmfR76N_Qk6E3S6Wx2zXnZyqZjlma6c1tpuOVTIwK_FjCb-UIpFCczlgIi_nmN8-UQYbibVUVVd_SXMCNU3zXPRAvRXjqpvgnupW4nNg93js7m2lJDggN0qpSRz98_9pcUo9zn7SGDB2BKq82U3RaVN_WAXemoDdXAYR4ePUA2UsFFkO1zcgDJz84dVkzqRK4rDCSu3qXS0CDNK2XYZNTfA7tUyJMzuE3RDLqSV7ckjJmCtSVB2UYTlT_KeWjIEbY2vUzto88jsIBz_RbngUTsRT_w7-vY9Wt4VK_-alu3GR1H7Q8YkU_VCQW_SQ3hpDg"
+
+var Rx = RootResolver{}
+
+type RootResolver struct{}
+
+func (r *RootResolver) Ping(ctx context.Context) string {
+	return "pong"
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := context.TODO()
@@ -55,8 +70,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 // }
 
 func main() {
-	// Connect to Cloud SQL:
-	log.Print("connecting to Cloud SQL")
+	// Connect to Postgres:
 	var err error
 	db, err = sql.Open("cloudsqlpostgres", fmt.Sprintf(`
 		host=codex-ef322:us-west1:codex-db
@@ -69,24 +83,27 @@ func main() {
 	defer db.Close()
 
 	// Setup Firebase (1 of 2):
-	log.Print("setting up Firebase Admin")
-	var err error
 	opt := option.WithCredentialsFile(".secret/firebase-admin-sdk.json")
 	app, err = firebase.NewApp(context.TODO(), nil, opt)
 	must(err, "error due to firebase.NewApp")
-	// Setup Firebase (2 of 2):
 	client, err = app.Auth(context.TODO())
 	must(err, "error due to app.Auth")
 
+	// Parse GraphQL schema:
+	bstr, err := ioutil.ReadFile("schema.graphql")
+	must(err, "error due to ioutil.ReadFile")
+	schema, err = graphql.ParseSchema(string(bstr), &RootResolver{})
+	must(err, "error due to graphql.ParseSchema")
+
 	// Listen and serve:
+	log.Print("ok")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	log.Printf("serving on port %s", port)
 	http.HandleFunc("/", handler)
 	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
-	log.Fatal(err)
+	must(err, "error due to http.ListenAndServe")
 }
 
 // // Postgres database:
