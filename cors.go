249bd10a59ba58e1db7900ae27c4faeab40ec7f4
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-16 18:36:35 +0900

    Fixed backend to support public API access

--- cors.go
+++ cors.go
@@ -12,14 +12,13 @@ var Methods = []string{
 }
 
 var Headers = []string{
-	"Access-Control-Allow-Headers", // Needed?
+	"Access-Control-Allow-Headers",
 	"Authorization",
 	"Content-Type",
 	"X-Requested-With",
 }
 
 func enableCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Headers", strings.Join(Headers, ", "))
 	w.Header().Set("Access-Control-Allow-Methods", strings.Join(Methods, ", "))
 	w.Header().Set("Access-Control-Allow-Origin", "*")
