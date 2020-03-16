208e1cb55cfbe6fe7dadcb53ca9778ab299fa2cb
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-16 17:40:00 +0900

    Upgrade server to 0.1.3

--- main.go
+++ main.go
@@ -20,8 +20,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// // DELETEME
-// const idToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjhjZjBjNjQyZDQwOWRlODJlY2M5MjI4ZTRiZDc5OTkzOTZiNTY3NDAiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiWmF5ZGVrIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BT2gxNEdpRENxc1Y5bmU1MjdZNnpwejUxOE40VHNNY3J0VU5TNWtubXdSNXV3IiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2NvZGV4LWVmMzIyIiwiYXVkIjoiY29kZXgtZWYzMjIiLCJhdXRoX3RpbWUiOjE1ODQxMjQyMTEsInVzZXJfaWQiOiJ0WG14SGJpUElDYkRCdDFPckFFbHJUSERTQTQyIiwic3ViIjoidFhteEhiaVBJQ2JEQnQxT3JBRWxyVEhEU0E0MiIsImlhdCI6MTU4NDEyNDIxMywiZXhwIjoxNTg0MTI3ODEzLCJlbWFpbCI6InpheWRla2RvdGNvbUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjEwNzI1Mjk3Mzk2NDE0NDA1NzU3MyJdLCJlbWFpbCI6WyJ6YXlkZWtkb3Rjb21AZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZ29vZ2xlLmNvbSJ9fQ.pOsCxDnGDNedKB0Xs07-MWmfR76N_Qk6E3S6Wx2zXnZyqZjlma6c1tpuOVTIwK_FjCb-UIpFCczlgIi_nmN8-UQYbibVUVVd_SXMCNU3zXPRAvRXjqpvgnupW4nNg93js7m2lJDggN0qpSRz98_9pcUo9zn7SGDB2BKq82U3RaVN_WAXemoDdXAYR4ePUA2UsFFkO1zcgDJz84dVkzqRK4rDCSu3qXS0CDNK2XYZNTfA7tUyJMzuE3RDLqSV7ckjJmCtSVB2UYTlT_KeWjIEbY2vUzto88jsIBz_RbngUTsRT_w7-vY9Wt4VK_-alu3GR1H7Q8YkU_VCQW_SQ3hpDg"
+const UserIDKey = UserID("userID")
 
 var (
 	// Postgres database
@@ -35,30 +34,38 @@ var (
 	schema *graphql.Schema
 )
 
-// Represents a GraphQL query or mutation.
-type Query struct {
-	UserID    string
-	Query     string
-	Variables map[string]interface{}
-}
-
 type UserID string
 
-const UserIDKey = UserID("userID")
+// Represents a GraphQL query or mutation.
+type Query struct {
+	OperationName string
+	Query         string
+	Variables     map[string]interface{}
+}
+
+// // Decode query (parse JSON):
+// var query Query
+// err := json.NewDecoder(r.Body).Decode(&query)
+// if err != nil {
+// 	log.Print(fmt.Errorf("error due to json.NewDecoder.Decode: %w", err))
+// 	RespondServerError(w)
+// 	return
+// }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// CORS:
+	// Cross-origin resource sharing (CORS):
 	enableCORS(w)
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		return
 	}
-	// Check auth:
-	//
-	// TODO: Extract to hasAuth
-	userID := ""
-	if r.Header.Get("Authorization") != "" {
-		idToken := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+	// Get current user ID (authenticated):
+	var userID string
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		// No-op; defer to query or mutation
+	} else {
+		idToken := strings.TrimPrefix(authHeader, "Bearer ")
 		token, err := client.VerifyIDToken(context.TODO(), idToken)
 		if err != nil {
 			log.Print(fmt.Errorf("error due to client.VerifyUserID: %w", err))
@@ -67,38 +74,45 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 		userID = token.UID
 	}
-	// Parse query:
-	var query Query // query := Query{UserID: token.UID}
-	err := json.NewDecoder(r.Body).Decode(&query)
+	// Read request body:
+	dataIn, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Print(fmt.Errorf("error due to json.NewDecoder.Decode: %w", err))
+		log.Print(fmt.Errorf("error due to ioutil.ReadAll: %w", err))
 		RespondServerError(w)
 		return
 	}
-	// Execute query and respond:
-	ctx := context.TODO()
-	ctx = context.WithValue(ctx, UserIDKey, userID)
-	res := schema.Exec(ctx, query.Query, "", query.Variables)
-	if res.Errors != nil {
-		log.Printf("res.Errors=%+v", res.Errors)
+	// Decode query (parse JSON):
+	var query Query
+	err = json.Unmarshal(dataIn, &query)
+	if err != nil {
+		log.Print(fmt.Errorf("error due to json.Unmarshal: %w", err))
+		RespondServerError(w)
+		return
 	}
-	bstr, err := json.MarshalIndent(res, "", "\t")
+	// Execute query:
+	log.Print("query: ", string(dataIn))
+	ctx := context.WithValue(context.TODO(), UserIDKey, userID)
+	resp := schema.Exec(ctx, query.Query, query.OperationName, query.Variables)
+	if resp.Errors != nil {
+		log.Printf("error due to schema.Exec: %+v", resp.Errors)
+	}
+	// Encode response:
+	dataOut, err := json.Marshal(resp)
 	if err != nil {
 		log.Print(fmt.Errorf("error due to json.MarshalIndent: %w", err))
 		RespondServerError(w)
 		return
 	}
-	fmt.Fprintln(w, string(bstr))
+	// Done:
+	fmt.Fprintln(w, string(dataOut))
 }
 
-// GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/service-account-file.json"
-// app, err := firebase.NewApp(context.Background(), nil)
 func main() {
 	var err error
 	/*
 	 * Postgres
 	 */
-	log.Print("setting up database")
+	log.Print("setting up postgres")
 	db, err = sql.Open("cloudsqlpostgres", fmt.Sprintf(`
 		host=codex-ef322:us-west1:codex-db
 		user=postgres
@@ -112,7 +126,7 @@ func main() {
 	must(err, "crash due to db.QueryRow")
 	defer db.Close()
 	/*
-	 * Firebase
+	 * Firebase auth
 	 */
 	log.Print("setting up firebase auth")
 	opt := option.WithCredentialsFile("secret/firebase-admin-sdk.json")
@@ -121,7 +135,7 @@ func main() {
 	client, err = app.Auth(context.TODO())
 	must(err, "crash due to app.Auth")
 	/*
-	 * GraphQL
+	 * Schema
 	 */
 	log.Print("setting up schema")
 	bstr, err := ioutil.ReadFile("schema.graphql")
@@ -138,6 +152,9 @@ func main() {
 		port = "8080"
 	}
 	http.HandleFunc("/graphql", handler)
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	must(err, "crash due to http.ListenAndServe")
 }
